9798ca9d410203f706bb5060fbb35a618e2f9eb1
mrmikemcguire <moonshotmike@gmail.com>
2019-08-17 17:08:08 -0600

    initial commit

---
+++ Output Examples/src/OutputExamples.java
@@ -0,0 +1,20 @@
+
+public class OutputExamples
+	{
+
+	public static void main(String[] args)
+		{
+		System.out.print("Hello");
+		System.out.println("World");
+		System.out.print("Hello");
+		System.out.print("World");
+		System.out.println("Hello");
+		System.out.println("World\n");
+		
+		System.out.println("Mullen");
+		System.out.println("\"Mullen\"");
+		System.out.println("Mullen\tMullen\tMullen\n");
+		System.out.println( " \\ \\\\ \\\\\\ \\\\\\\\" ); // it takes 2 to make 1
+		}
+
+	}
